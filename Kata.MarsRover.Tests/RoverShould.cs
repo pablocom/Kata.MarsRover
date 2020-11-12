baf252c678ab9710bae8008340a0c2e1de141619
Pablo <p.c.ramirez5150@gmail.com>
2020-11-12 20:38:29 +0100

    refactor: Adds movements

--- Kata.MarsRover.Tests/RoverShould.cs
+++ Kata.MarsRover.Tests/RoverShould.cs
@@ -16,7 +16,7 @@ namespace Kata.MarsRover.Tests
         [TestCase("RR", "0:0:S")]
         [TestCase("RRR", "0:0:W")]
         [TestCase("RRRR", "0:0:N")]
-        public void RotatesRight(string command, string expectedPosition)
+        public void RotateRight(string command, string expectedPosition)
         {
             Assert.That(rover.Execute(command), Is.EqualTo(expectedPosition));
         }
@@ -25,7 +25,51 @@ namespace Kata.MarsRover.Tests
         [TestCase("LL", "0:0:S")]
         [TestCase("LLL", "0:0:E")]
         [TestCase("LLLL", "0:0:N")]
-        public void RotatesLeft(string command, string expectedPosition)
+        public void RotateLeft(string command, string expectedPosition)
+        {
+            Assert.That(rover.Execute(command), Is.EqualTo(expectedPosition));
+        }
+
+        [TestCase("M", "0:1:N")]
+        [TestCase("MMM", "0:3:N")]
+        public void MoveUp(string command, string expectedPosition)
+        {
+            Assert.That(rover.Execute(command), Is.EqualTo(expectedPosition));
+        }
+        
+        [TestCase("MMMMMMMMM", "0:9:N")]
+        [TestCase("MMMMMMMMMM", "0:0:N")]
+        [TestCase("MMMMMMMMMMM", "0:1:N")]
+        public void WrapFromTopToBottomWhenMovingNorth(string command, string expectedPosition)
+        {
+            Assert.That(rover.Execute(command), Is.EqualTo(expectedPosition));
+        }
+        
+        [TestCase("RM", "1:0:E")]
+        [TestCase("RMMM", "3:0:E")]
+        public void MoveRight(string command, string expectedPosition)
+        {
+            Assert.That(rover.Execute(command), Is.EqualTo(expectedPosition));
+        }
+        
+        [TestCase("RMMMMMMMMM", "9:0:E")]
+        [TestCase("RMMMMMMMMMM", "0:0:E")]
+        [TestCase("RMMMMMMMMMMM", "1:0:E")]
+        public void WrapFromRightToLeftWhenMovingEast(string command, string expectedPosition)
+        {
+            Assert.That(rover.Execute(command), Is.EqualTo(expectedPosition));
+        }
+        
+        [TestCase("LM", "9:0:W")]
+        [TestCase("LMMMMM", "5:0:W")]
+        public void MoveLeft(string command, string expectedPosition)
+        {
+            Assert.That(rover.Execute(command), Is.EqualTo(expectedPosition));
+        }
+        
+        [TestCase("LLM", "0:9:S")]
+        [TestCase("LLMMMMM", "0:5:S")]
+        public void MoveSouth(string command, string expectedPosition)
         {
             Assert.That(rover.Execute(command), Is.EqualTo(expectedPosition));
         }
--- Kata.MarsRover/Rover.cs
+++ Kata.MarsRover/Rover.cs
@@ -2,24 +2,61 @@
 {
     public class Rover
     {
-        private Direction direction;
-
-        public Rover()
-        {
-            direction = Direction.North;
-        }
+        private const int MaxHeight = 10;
+        private const int MaxWidth = 10;
+        
+        private Direction direction = Direction.North;
+        private Coordinate coordinate = new Coordinate(0, 0);
 
         public string Execute(string commands)
         {
             foreach (var command in commands.ToCharArray())
             {
-                if (command == 'R')
-                    direction = direction.Right();
-                if (command == 'L')
-                    direction = direction.Left();
+                if (command == 'R') direction = direction.Right();
+                if (command == 'L') direction = direction.Left();
+                if (command == 'M') coordinate = Move();
             }
 
-            return $"0:0:{direction.Value}";
+            return $"{coordinate.X}:{coordinate.Y}:{direction.Value}";
         }
+
+        private Coordinate Move()
+        {
+            var y = coordinate.Y;
+            var x = coordinate.X;
+            
+            if (direction == Direction.North)
+                y = (y + 1) % MaxHeight;
+
+            if (direction == Direction.East)
+                x = (x + 1) % MaxWidth;
+
+            if (direction == Direction.West)
+                if (x > 0)
+                    x -= 1;
+                else
+                    x = 9;
+
+            if (direction == Direction.South)
+                if (y > 0)
+                    y -= 1;
+                else
+                    y = 9;
+
+            return new Coordinate(x, y);
+        }
+    }
+    public class Coordinate
+    {
+        public int X { get; }    
+        public int Y { get; private set; }
+
+        public Coordinate(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void MoveUp() => this.Y++;
     }
 }
