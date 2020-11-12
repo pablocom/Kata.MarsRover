d941478e59cd7aba32bd551af7071cdc3380fb1c
Pablo <p.c.ramirez5150@gmail.com>
2020-11-12 19:47:17 +0100

    refactor: Moves direction to another class file

---
+++ Kata.MarsRover/Direction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Kata.MarsRover
+{
+    public class Direction 
+    {
+        public static readonly Direction North = new Direction("N", "W", "E");
+        public static readonly Direction East = new Direction("E", "N", "S");
+        public static readonly Direction South = new Direction("S", "E", "W");
+        public static readonly Direction West = new Direction("W", "S", "N");
+        
+        private static readonly IEnumerable<Direction> AllDirections = new List<Direction>
+        {
+            North, East, South, West
+        };
+        
+        public string Value { get; }
+        private readonly string leftValue;
+        private readonly string rightValue;
+
+        private Direction(string value, string leftValue, string rightValue)
+        {
+            this.Value = value;
+            this.leftValue = leftValue;
+            this.rightValue = rightValue;
+        }
+
+        public Direction Right() => AllDirections.First(direction => direction.Value == this.rightValue);
+        public Direction Left() => AllDirections.First(direction => direction.Value == this.leftValue);
+    }
+}
--- Kata.MarsRover/Rover.cs
+++ Kata.MarsRover/Rover.cs
@@ -1,12 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace Kata.MarsRover
+﻿namespace Kata.MarsRover
 {
     public class Rover
     {
-        private Direction direction = Direction.North;
+        private Direction direction;
+
+        public Rover()
+        {
+            direction = Direction.North;
+        }
 
         public string Execute(string commands)
         {
@@ -20,32 +21,5 @@ namespace Kata.MarsRover
 
             return $"0:0:{direction.Value}";
         }
-    }
-    
-    public class Direction 
-    {
-        public static readonly Direction North = new Direction("N", "W", "E");
-        public static readonly Direction East = new Direction("E", "N", "S");
-        public static readonly Direction South = new Direction("S", "E", "W");
-        public static readonly Direction West = new Direction("W", "S", "N");
-        
-        private static readonly IEnumerable<Direction> AllDirections = new List<Direction>
-        {
-            North, East, South, West
-        };
-        
-        public string Value { get; }
-        private readonly string leftValue;
-        private readonly string rightValue;
-
-        private Direction(string value, string leftValue, string rightValue)
-        {
-            this.Value = value;
-            this.leftValue = leftValue;
-            this.rightValue = rightValue;
-        }
-
-        public Direction Right() => AllDirections.First(direction => direction.Value == this.rightValue);
-        public Direction Left() => AllDirections.First(direction => direction.Value == this.leftValue);
     }
 }
